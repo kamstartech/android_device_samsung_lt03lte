d1ae84cd97d8a2ca5b94e0181d7664e1c9763b70
jimmykamanga <jimmykamanga@gmail.com>
2026-06-22 17:20:07 +0200

    lt03lte: drop custom mkbootimg.mk, use kernel-appended DTB
    zImage-dtb has all DTBs concatenated by the kernel build itself.
    dtbToolCM no longer exists in LineageOS 22.2 and the separate
    dt.img + --dt mkbootimg path is obsolete.
    
    Change-Id: I50cbe2ac8d4daf804f3e56d7f7798fc7571002ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,8 +31,6 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 # Kernel
 TARGET_KERNEL_CLANG_COMPILE := false
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/releasetools/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
